499430a09bc423e25039fbb7bcc43833407d99da
Bhavleen <bhavleen@mobifyi.com>
2022-07-03 11:41:21 +0530

    Updated Error Msg

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -154,9 +154,9 @@
       //   }
       //   error_msg += '<br>';
       // }
-      if (data.responseText) {
-        error_msg += data.responseText;
-      }
+      // if (data.responseText) {
+      //   error_msg += data.responseText;
+      // }
       this_form.find('.loading').slideUp();
       this_form.find('.error-message').slideDown().html(error_msg);
     });
